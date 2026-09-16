f6a02c724361073ed081c711555b4bc26502ff38
niro1n <syahfrinorezky@gmail.com>
2026-09-16 16:41:26 +0800

    feat: add role states to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,21 +25,39 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'voter',
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
+    }
+
+    public function superAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'super_admin',
+        ]);
+    }
+
+    public function voter(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'voter',
         ]);
     }
 }
