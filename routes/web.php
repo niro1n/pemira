7341ec984cc585383a724880bd9bba5e0a65f265
niro1n <syahfrinorezky@gmail.com>
2026-09-16 16:50:32 +0800

    feat: register password request and reset routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\Auth\ForgotPassword;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
+use App\Livewire\Auth\ResetPassword;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +11,8 @@ Route::view('/', 'pages.home')->name('home');
 
 Route::get('/login', Login::class)->middleware('guest')->name('login');
 Route::get('/register', Register::class)->middleware('guest')->name('register');
+Route::get('/forgot-password', ForgotPassword::class)->middleware('guest')->name('password.request');
+Route::get('/reset-password/{token}', ResetPassword::class)->middleware('guest')->name('password.reset');
 
 Route::get('/profile', function () {
     if (Auth::user()->canAccessAdminPanel()) {
