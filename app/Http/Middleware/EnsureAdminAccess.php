6a1b063c5a1b2dd27b0beca16deff3285afb50ee
niro1n <syahfrinorezky@gmail.com>
2026-09-16 16:41:07 +0800

    feat: add ensure admin access middleware

---
+++ app/Http/Middleware/EnsureAdminAccess.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAdminAccess
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if (! $user) {
+            return redirect()->guest(route('login'));
+        }
+
+        if (! $user->canAccessAdminPanel()) {
+            abort(403, 'Akses ditolak.');
+        }
+
+        return $next($request);
+    }
+}
